81691bfa47cc1b94a0b2deb9b8573c8c76ae3677
James Wesley Goedert <jwgoedert@gmail.com>
2021-09-16 17:00:19 -0500

    Added JS functions, conditions, and loops

--- scripts.js
+++ scripts.js
@@ -17,11 +17,43 @@ var fullPerson = {
 console.log(fullPerson.favoriteNumber, fullPerson.lastName);
 //array example
 var funClub = ['Vanessa', 'Michael', 'James', 'Bob', 'Amanda'];
-console.log(funClub[0]);
 
 //For loop uses (start value variable; end condition; counter)
-for(var i = 0; i < funClub.length; i = i + 1){
-    console.log(funClub[i]);
-    // console.log(i);
-//action
+for(var i = 0; i < funClub.length; i++){
+    // console.log(funClub[i] + " is in the funnest fall class!");
+}
+
+//conditionals
+
+let funClubList = [{
+        firstName: 'James',
+        lastName: 'Goedert',
+        favoriteNumber: 35,
+        fromOmaha: true,
+    }, {
+        firstName: 'Michael',
+        lastName: 'Caliguiri',
+        favoriteNumber: 31,
+        fromOmaha: false,
+    },{
+        firstName: 'Vanessa',
+        lastName: 'Martinez-Hernandez',
+        favoriteNumber: 10,
+        fromOmaha: true,
+    }
+]
+
+function isFromOmaha(person){
+    if (person.fromOmaha == true) {
+        console.log(person.firstName + ' is from Omaha');
+    } else if (person.fromOmaha !== true) {
+        console.log(person.firstName + ' is not from Omaha');
+    } else {
+        console.log('that information was not provided');
+    }
+}
+
+for (var i = 0; i < funClubList.length; i++) {
+    // console.log(funClub[i] + " is in the funnest fall class!");
+    isFromOmaha(funClubList[i]);
 }
